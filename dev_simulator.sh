10c4373970fd07766a7ea0bb5a70aa15ed30a13d
Johan-py <johanenvdev@gmail.com>
2026-03-13 15:11:11 -0400

    fix: test dev_simulator

--- dev_simulator.sh
+++ dev_simulator.sh
@@ -3,13 +3,15 @@
 BRANCH=$1
 COMMITS=$2
 
+SAFE_BRANCH=$(echo $BRANCH | tr '/' '_')
+
 git checkout $BRANCH || git checkout -b $BRANCH
 
 for i in $(seq 1 $COMMITS)
 do
-  echo "$BRANCH change $i $(date)" >> dev_${BRANCH}.txt
+  echo "$BRANCH change $i $(date)" >> dev_${SAFE_BRANCH}.txt
 
-  git add dev_${BRANCH}.txt
+  git add dev_${SAFE_BRANCH}.txt
   git commit -m "$BRANCH commit $i"
 
   git push origin $BRANCH
