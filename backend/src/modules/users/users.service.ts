6ffced927a691339484ddc916c8b835d59305d34
Jose <joseadrianvillazonrojas@gmail.com>
2026-03-27 18:51:28 -0400

    miaPassConfi

--- backend/src/modules/users/users.service.ts
+++ backend/src/modules/users/users.service.ts
@@ -11,21 +11,6 @@ export const createUserService = async (data: payload) => {
   if (!data.name) {
     throw new Error('Name is required')
   }
-    if (!data.password) {
-    throw new Error('Password is required')
-  }
-
-  if (!data.confirmPassword) {
-    throw new Error('Confirm password is required')
-  }
-
-  if (data.password !== data.confirmPassword) {
-    throw new Error('Las contraseñas no coinciden')
-  }
-  
-  const {confirmPassword, ...userData} = data
 
   return createUserRepository(data)
-
-
 }
