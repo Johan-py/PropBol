0b88074fe6c156b277d867ce2d80aaacaa798110
Alison1205 <alison.mamani.yujra1@gmail.com>
2026-04-01 01:19:49 -0400

    Mi aporte HU4 Alison

--- backend/prisma.config.ts
+++ backend/prisma.config.ts
@@ -1,18 +1,17 @@
-import { defineConfig } from '@prisma/config'
+import { defineConfig, env } from '@prisma/config'
 import 'dotenv/config'
 
 console.log('DATABASE_URL:', process.env.DATABASE_URL)
 
 export default defineConfig({
-  earlyAccess: true,
   schema: './prisma/schema.prisma',
+
   datasource: {
-    url: process.env.DATABASE_URL!
+    url: env('DATABASE_URL'),        // ← forma recomendada
   },
-  migrate: {
-    adapter: async () => {
-      const { PrismaPg } = await import('@prisma/adapter-pg')
-      return new PrismaPg({ connectionString: process.env.DATABASE_URL! })
-    }
+
+  // Opcional: configura la carpeta de migraciones
+  migrations: {
+    path: './prisma/migrations',     // si quieres especificar
   }
 })
