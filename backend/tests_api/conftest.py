cbaf836a6cdacee8e8ed31b5205f9557215ff4d3
JonatanZambrana <oink00234@gmail.com>
2026-04-10 00:50:48 -0400

    fix: sincronizacion total de URL de Render

--- backend/tests_api/conftest.py
+++ backend/tests_api/conftest.py
@@ -3,7 +3,6 @@ import requests
 
 @pytest.fixture(scope="session")
 def base_url():
-    # USAMOS RENDER PORQUE VERCEL DA 404
     return "https://propbol-tsiq.onrender.com"
 
 @pytest.fixture(scope="session")
