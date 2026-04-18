7b787560eacf05fdb6b57eb70f06799739266e42
Tarque Guzman Oscar <tarqueoscar1@gmail.com>
2026-04-18 10:16:48 -0400

    feat(hu08): crear ruta base de Mis Zonas

---
+++ frontend/src/app/profile/mis-zonas/page.tsx
@@ -0,0 +1,9 @@
+import MisZonas from '@/components/profile/MisZonas/MisZonas'
+
+export default function MisZonasPage() {
+    return (
+        <div className="p-6">
+            <MisZonas />
+        </div>
+    )
+}
