4071bf20ee25d918815c559748d1fb1869ecba00
William-Vela-Uribe <velauribew@gmail.com>
2026-04-18 15:14:47 -0600

    feat: implementar guardado de fecha de nacimiento

--- backend/src/api/users/index.ts
+++ backend/src/api/users/index.ts
@@ -1,6 +1,7 @@
 import express from 'express'
 import cors from 'cors'
 import { registerController, loginController } from '../../modules/auth/auth.controller.js'
+import perfilRoutes from '../../modules/perfil/perfil.routes.js'
 
 const app = express()
 
@@ -9,6 +10,7 @@ app.use(cors())
 
 app.post('/api/auth/register', registerController)
 app.post('/api/auth/login', loginController)
+app.use('/api/perfil', perfilRoutes)
 
 const PORT = 5000
 
