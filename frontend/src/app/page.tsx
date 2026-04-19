22171f52870166c4fd5f3c413f8c1af8543f50e8
Giovani-Quiroz <giovaniquiroz066@gmail.com>
2026-04-19 13:11:49 -0400

    refactor(home): correccion de las rutas de importación e integración de VisualFiltersSection

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,14 +1,7 @@
-<<<<<<< HEAD
-import { HomeBanner } from '@/components/home/HomeBanner'
-import { HomeCarousel } from '@/components/home/HomeCarousel'
-import ExploreSection from '@/components/layout/ExploreSection'
-import FilterPanel from '@/components/rentals/FilterPanel'
-=======
 import { HomeBanner } from "@/components/home/HomeBanner";
 import { HomeCarousel } from "@/components/home/HomeCarousel";
 import ExploreSection from "@/components/layout/ExploreSection";
 import VisualFiltersSection from "@/components/VisualFilters/VisualFiltersSection";
->>>>>>> 3ce95ea (refactor(Home): actualizar nueva seccion de filtros visuales)
 interface BannerData {
   id: number
   urlImagen: string
@@ -51,33 +44,17 @@ export default async function Home() {
       )}
 
       {/* CONTENEDOR PRINCIPAL */}
-<<<<<<< HEAD
-      <div className="w-full px-2 md:px-6 py-12">
-        <div className="flex flex-col-reverse md:flex-row items-start">
-          {/* FILTER PANEL */}
-          <div className="w-full md:w-[240px] lg:w-[260px] shrink-0">
-            <FilterPanel />
-          </div>
+      <div className="w-full px-2 md:px-6 py-12 flex flex-col items-center gap-12">
+       {/* EXPLORE SECTION */}
+       <section className="w-full flex justify-center">
+         <ExploreSection />
+       </section>
 
-          {/* EXPLORE SECTION */}
-          <section className="flex-1 w-full md:pl-20 -mt-16 md:mt-0">
-            <ExploreSection />
-          </section>
-        </div>
-      </div>
-=======
-      <div className="w-full px-2 md:px-6 py-12 flex flex-col items-center gap-12">
-        {/* EXPLORE SECTION */}
-        <section className="w-full flex justify-center">
-          <ExploreSection />
-        </section>
-
-        {/* SECCIÓN 2: TUS NUEVOS FILTROS VISUALES (HU-01) */}
-        <section className="w-full">
-          <VisualFiltersSection />
-        </section>
-      </div>
->>>>>>> 3ce95ea (refactor(Home): actualizar nueva seccion de filtros visuales)
+         {/* FILTROS VISUALES */}
+       <section className="w-full">
+         <VisualFiltersSection />
+       </section>
+     </div>
     </main>
   )
 }
