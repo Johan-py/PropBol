c090d54dbaa751360ee6c50250db40cf02eff18b
PanaShusco <joelllusco1@gmail.com>
2026-04-20 19:15:24 -0400

    feat: se agrego detalle de notificacion

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,7 +4,7 @@ import express from 'express'
 import cors from 'cors'
 import { env } from './config/env.js'
 import type { Request, Response } from 'express'
-import zonaRoutes from "./modules/perfil/zonaUsario.routes.js";
+import zonaRoutes from './modules/perfil/zonaUsario.routes.js'
 // --------------------
 // CONTROLLERS
 // --------------------
@@ -13,6 +13,8 @@ import {
   createNotificationController,
   deleteNotificationController,
   getNotificationsController,
+  getNotificationByIdController,
+  archiveNotificationController,
   getUnreadCountController,
   markAllNotificationsAsReadController,
   markNotificationAsReadController
@@ -48,13 +50,11 @@ import {
   StratGoogleLoginController,
   StartGoogleRegisterController
 } from './modules/auth/google/google.controller.js'
- 
-
 
 import multimediaRoutes from './modules/multimedia/multimedia.routes.js'
 import publicacionRoutes from './modules/publicacion/publicacion.routes.js'
 import router from './modules/registro-publicacion/publicacion.routes.js'
-import securityRoutes from "./routes/security.routes.js";
+import securityRoutes from './routes/security.routes.js'
 // --------------------
 // LEGACY
 // --------------------
@@ -69,7 +69,7 @@ import { verifyEmailTransport } from './lib/email.service.js'
 
 // FAVORITES
 
-import favoritesRoutes from "./modules/favorites/favorites.routes.js";
+import favoritesRoutes from './modules/favorites/favorites.routes.js'
 // --------------------
 // SERVER
 // --------------------
@@ -85,7 +85,6 @@ const allowedOrigins = [
   'http://localhost:3000',
   'http://localhost:3001',
   'http://localhost:2000'
-
 ]
 
 // Middleware CORS global
@@ -109,8 +108,8 @@ app.use('/uploads', express.static(path.resolve('uploads')))
 // --------------------
 // RUTAS LEGACY
 // --------------------
-app.post("/api/auth/forgot-password", forgotPasswordController);
-app.post("/api/auth/reset-password", resetPasswordController);
+app.post('/api/auth/forgot-password', forgotPasswordController)
+app.post('/api/auth/reset-password', resetPasswordController)
 app.use('/api/auth-legacy', authRoutes)
 app.get('/api/users/:id/publicaciones/free', authMiddleware, (_req, res) => {
   res.json({ restantes: 2 })
@@ -120,14 +119,14 @@ app.use('/api/publicaciones-legacy', publicacionesRoutes)
 // --------------------
 // RUTAS PRINCIPALES
 // --------------------
-app.use("/api/publicaciones", publicacionRoutes);
-app.use("/api/publicaciones", multimediaRoutes);
-app.use("/api/perfil", correoverificacionRoutes);
-app.use("/api/perfil/usuario", perfilRoutes);
-app.use("/api/perfil/zonas", zonaRoutes);
-app.use("/api", router);
-app.use("/api/security", securityRoutes);
-app.use("/api/favorites", favoritesRoutes);
+app.use('/api/publicaciones', publicacionRoutes)
+app.use('/api/publicaciones', multimediaRoutes)
+app.use('/api/perfil', correoverificacionRoutes)
+app.use('/api/perfil/usuario', perfilRoutes)
+app.use('/api/perfil/zonas', zonaRoutes)
+app.use('/api', router)
+app.use('/api/security', securityRoutes)
+app.use('/api/favorites', favoritesRoutes)
 // --------------------
 // MOCK / TEST
 // --------------------
@@ -135,19 +134,19 @@ app.post('/api/users', (req, res) => {
   const user = req.body
   res.json({ message: 'User created', user })
 })
-app.use('/api/perfil/zonas', zonaRoutes);
+app.use('/api/perfil/zonas', zonaRoutes)
 
 // --------------------
 // AUTH
 // --------------------
-app.post("/api/auth/register", registerController);
-app.post("/api/auth/login", loginController);
-app.post("/api/auth/logout", logoutController);
-app.post("/api/auth/verify-register", verifyRegisterCodeController);
-app.get("/api/auth/me", getMeController);
-app.get("/api/auth/google/login", StratGoogleLoginController);
-app.get("/api/auth/google/register", StartGoogleRegisterController);
-app.get("/api/auth/google/callback", googleCallbackController);
+app.post('/api/auth/register', registerController)
+app.post('/api/auth/login', loginController)
+app.post('/api/auth/logout', logoutController)
+app.post('/api/auth/verify-register', verifyRegisterCodeController)
+app.get('/api/auth/me', getMeController)
+app.get('/api/auth/google/login', StratGoogleLoginController)
+app.get('/api/auth/google/register', StartGoogleRegisterController)
+app.get('/api/auth/google/callback', googleCallbackController)
 //comentario
 
 // --------------------
@@ -188,9 +187,11 @@ app.get('/api/properties/inmuebles', propertiesController.getAll)
 app.post('/notificaciones', requireAuth, createNotificationController)
 app.get('/notificaciones', requireAuth, getNotificationsController)
 app.get('/notificaciones/unread-count', requireAuth, getUnreadCountController)
+app.get('/notificaciones/:id', requireAuth, getNotificationByIdController)
 app.patch('/notificaciones/:id/read', requireAuth, markNotificationAsReadController)
 app.patch('/notificaciones/read-all', requireAuth, markAllNotificationsAsReadController)
 app.delete('/notificaciones/:id', requireAuth, deleteNotificationController)
+app.patch('/notificaciones/:id/archivar', requireAuth, archiveNotificationController)
 
 // --------------------
 // PUBLICACIONES MOCK
