c66cedf08d340d0fd00c198216f25ac3204b973f
Miguel Angel Massi Geronimo <miguelangelmassigeronimo@gmail.com>
2026-04-21 23:48:49 -0400

    feat: inicializar blogsService con métodos de consulta básicas

---
+++ backend/src/modules/blogs/blogs.service.ts
@@ -0,0 +1,19 @@
+import { blogsRepository, comentariosRepository } from './blogs.repository.js'
+
+// BLOGS SERVICE PE
+
+export const blogsService = {
+  async listar(params: { categoria_id?: number; page?: number; limit?: number }) {
+    return blogsRepository.findAll(params)
+  },
+
+  async misBlogs(usuario_id: number) {
+    return blogsRepository.findByUserId(usuario_id)
+  },
+
+  async obtener(id: number) {
+    const blog = await blogsRepository.findById(id)
+    if (!blog) throw new Error('BLOG_NOT_FOUND')
+    return blog
+  }
+}
