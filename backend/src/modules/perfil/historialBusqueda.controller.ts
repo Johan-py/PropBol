2f5e0f7e0d59dfade1883c620bb679693c9b4143
Esther2419 <romeromenachoesther@gmail.com>
2026-04-23 13:43:14 -0400

    feat: Sincronización de historial de búsqueda y soporte para eliminar términos (#524)
    * feat(backend): se imp[lemento logica de bd para historial de busqueda
    
    * feat(backend): se implemento la rutas de backend para historial de busqueda
    
    * feat(frontend): se implemento logica para recibir la logica de backend

--- backend/src/modules/perfil/historialBusqueda.controller.ts
+++ backend/src/modules/perfil/historialBusqueda.controller.ts
@@ -44,3 +44,26 @@ export const guardarBusqueda = async (req: Request, res: Response) => {
         res.status(500).json({ error: "Error al registrar la búsqueda" });
     }
 };
+
+export const eliminarBusqueda = async (req: Request, res: Response) => {
+    try {
+        const usuarioId = req.user?.id;
+        const termino = req.params.termino as string;
+
+        if (!usuarioId || !termino) {
+            return res.status(400).json({ message: "Usuario o término faltante" });
+        }
+
+        await prisma.historial_busqueda.deleteMany({
+            where: {
+                usuarioid: usuarioId,
+                termino: termino
+            }
+        });
+
+        res.json({ message: "Búsqueda eliminada correctamente" });
+    } catch (error) {
+        console.error("Error al eliminar búsqueda:", error);
+        res.status(500).json({ error: "Error al eliminar la búsqueda" });
+    }
+};
