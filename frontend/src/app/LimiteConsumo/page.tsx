772f77a72f67da68612768e9f4d6e83e717b415c
AlejandroPardoB <pardobautistaalejandro@gmail.com>
2026-04-27 21:14:04 -0400

    fix:correccion de texto

--- frontend/src/app/LimiteConsumo/page.tsx
+++ frontend/src/app/LimiteConsumo/page.tsx
@@ -226,7 +226,7 @@ export default function ConsumoPage() {
               {data.limite}
             </h3>
             <p className="text-xl font-bold text-gray-500 uppercase tracking-widest mt-2">
-              Límite Total del Plan
+              Límite Total del Pla
             </p>
           </div>
       </div>
