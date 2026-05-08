ab3eda3a54e11b5789419efa65e4b0c27be7a5c0
OjedaValenteWilber08 <202203353@est.umss.edu>
2026-05-08 19:54:51 -0400

    feat(map): agregar función auxiliar isPointInPolygon para detectar puntos dentro del polígono(HU10)

--- frontend/src/components/map/ZonasOverlay.tsx
+++ frontend/src/components/map/ZonasOverlay.tsx
@@ -298,7 +298,23 @@ function labelIcon(nombre: string, isSelected: boolean, zoom: number, tipoZona: 
   })
 }
 
-
+function isPointInPolygon(lat: number, lng: number, polygon: [number, number][]): boolean {
+  let inside = false
+  
+  for (let i = 0, j = polygon.length - 1; i < polygon.length; j = i++) {
+    // Acceso correcto a las coordenadas
+    const xi = polygon[i][0]  // latitude
+    const yi = polygon[i][1]  // longitude
+    const xj = polygon[j][0]  // latitude
+    const yj = polygon[j][1]  // longitude
+    
+    const intersect = ((yi > lat) != (yj > lat)) &&
+      (lng < (xj - xi) * (lat - yi) / (yj - yi) + xi)
+    
+    if (intersect) inside = !inside
+  }
+  return inside
+}
 
 
 function ZonaInteractiva({
