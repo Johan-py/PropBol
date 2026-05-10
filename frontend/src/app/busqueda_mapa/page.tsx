31e7967ab09f87b93d09c2be2905ee96d35bfed9
CalepP <202401238@est.umss.edu>
2026-05-10 10:11:09 -0400

    feat(HU13): pasar coordenadas lat lng a PropertyRow en render movil

--- frontend/src/app/busqueda_mapa/page.tsx
+++ frontend/src/app/busqueda_mapa/page.tsx
@@ -1837,6 +1837,8 @@ function BusquedaMapaContent() {
                             onViewDetails={() => {
                               if (!isCompareMode) abrirDetallePropiedad(property.id)
                             }}
+                            lat={property.lat}
+                            lng={property.lng}
                           />
                         )}
                       </div>
