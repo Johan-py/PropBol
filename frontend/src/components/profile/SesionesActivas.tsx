5ee30f53c8727affde587737a6c0ac3c7bc23703
Alejandra Flores Requiz <alexaflorequiz@gmail.com>
2026-05-10 21:46:13 -0400

    style: add main content container

--- frontend/src/components/profile/SesionesActivas.tsx
+++ frontend/src/components/profile/SesionesActivas.tsx
@@ -1,6 +1,9 @@
 export default function ActiveSessions() {
   return (
     <div className="min-h-screen bg-[#EAEAEA] p-4">
+      <div className="bg-[#D9D9D9] rounded-sm p-6">
+      </div>
     </div>
+
   );
 }
