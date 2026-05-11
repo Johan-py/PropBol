347363ef14e6cb242364a55eb8d30545e8b81a6c
isra-jq <israeljimenezquinteros@gmail.com>
2026-05-10 22:28:22 -0400

    feat(exchange): crear UI base y skeleton de ExchangeRateBar (T8)

--- frontend/src/components/home/HomeExchangeSection.tsx
+++ frontend/src/components/home/HomeExchangeSection.tsx
@@ -1,12 +1,10 @@
-"use client";
+'use client'
 
-import { ExchangeRateBar } from "@/components/ExchangeRateBar";
-import { useExchangeRate } from "@/hooks/useExchangeRate";
+import { ExchangeRateBar } from '@/components/ExchangeRateBar'
+import { useExchangeRate } from '@/hooks/useExchangeRate'
 
 export default function HomeExchangeSection() {
-  const exchangeRateState = useExchangeRate();
+  const exchangeRateState = useExchangeRate()
 
-  return (
-    <ExchangeRateBar {...exchangeRateState} />
-  );
+  return <ExchangeRateBar {...exchangeRateState} />
 }
