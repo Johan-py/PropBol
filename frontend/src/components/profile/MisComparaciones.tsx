105c92919267a19159490fe0c99084457e850096
William-Vela-Uribe <velauribew@gmail.com>
2026-05-11 16:09:21 -0600

    feat: MisComparaciones carpeta

---
+++ frontend/src/components/profile/MisComparaciones.tsx
@@ -0,0 +1,17 @@
+'use client';
+
+import React from 'react';
+
+export default function MisComparaciones() {
+  return (
+    <div className="w-full p-10">
+      <h1 className="text-4xl font-bold text-black mb-2">
+        Historial de Comparaciones
+      </h1>
+
+      <p className="text-gray-600 text-lg font-medium">
+        Revisa y retorna tus análisis previos de propiedades.
+      </p>
+    </div>
+  );
+}
