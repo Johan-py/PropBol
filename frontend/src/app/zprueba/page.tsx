b2f83d697e309159868e441342fc18ca13e6c705
Vlady610 <cvladimir610@gmail.com>
2026-05-15 04:18:37 -0400

    fix: actualizando un pequeño cambio

--- frontend/src/app/zprueba/page.tsx
+++ frontend/src/app/zprueba/page.tsx
@@ -8,7 +8,7 @@ export default function WelcomeForm() {
     <div className="min-h-screen flex items-center justify-center bg-gray-100">
       <div className="bg-white p-8 rounded-2xl shadow-lg w-96">
         <h1 className="text-3xl font-bold text-center mb-6">
-          Bienvenido
+          Bienvenido todos
         </h1>
 
         <form className="space-y-4">
